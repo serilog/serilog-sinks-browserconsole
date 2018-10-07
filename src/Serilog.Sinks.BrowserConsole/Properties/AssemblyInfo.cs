4986d6757c8cebe37267dd8c3ebc4ad317354841
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-10-08 07:36:23 +1000

    Update package metadata

--- src/Serilog.Sinks.BrowserConsole/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.BrowserConsole/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: InternalsVisibleTo("Serilog.Sinks.BrowserConsole.Tests")]
