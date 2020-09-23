96c368943082f7adc02de9935e33404e8be3508a
Zachary Becknell <zbecknell@users.noreply.github.com>
2020-09-23 10:15:53 -0400

    Update license header for Blazor files

--- src/Serilog.Sinks.BrowserConsole/Sinks/BrowserConsole/ElementReferenceJsonConverter.cs
+++ src/Serilog.Sinks.BrowserConsole/Sinks/BrowserConsole/ElementReferenceJsonConverter.cs
@@ -1,3 +1,5 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 // Copyright 2020 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
